15f57afebf025e6b7a5b160b6523af4e617b0ca5
Rolake Olonisakin <rollyloni@gmail.com>
2022-06-30 12:58:19 -0700

    question 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,14 @@
 console.log("hello");
 console.log("hello again");
+
+//Given a 32-bit signed integer, reverse digits of an integer.
+
+function returnReverse(num) {
+  let stringedNum = num.toString();
+  let a = stringedNum.split("");
+  let b = a.reverse().join();
+
+  return b;
+}
+
+console.log(returnReverse(123));
